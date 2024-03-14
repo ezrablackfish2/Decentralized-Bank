67a04e73e551f7bb59e7976b04d53f4b80d173e6
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-03-14 11:46:00 +0300

    very good very good

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './Navbar';
 import Web3 from 'web3'
 import Tether from  '../truffle_abis/Tether.json';
+import RWD from  '../truffle_abis/RWD.json';
+import DecentralBank from  '../truffle_abis/DecentralBank.json';
 
 
 class App extends Component {
@@ -29,7 +31,7 @@ class App extends Component {
 	async loadBlockchainData() {
 		const web3 = window.web3
 		const account = await web3.eth.getAccounts()
-		this.setState({ account: account[1] })
+		this.setState({ account: account[0] })
 		const networkId = await web3.eth.net.getId()
 
 		
@@ -39,10 +41,30 @@ class App extends Component {
 			this.setState({ tether })
 			let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
 			this.setState({tetherBalance: tetherBalance.toString()})
-			console.log({balance: tetherBalance});
 		} else {
 			window.alert('Error! Tether contract not deployed - no detected network!')	
 		}
+
+		const rwdData = RWD.networks[networkId]
+		if (rwdData) {
+			const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
+			this.setState({ rwd })
+			let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
+			this.setState({ rwdBalance: rwdBalance.toString()})
+		} else {
+			window.alert('Error! RWD contract not deployed - no detected network!')	
+		}
+
+		const decentralBankData = DecentralBank.networks[networkId]
+		if (decentralBankData) {
+			const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
+			this.setState({ decentralBank })
+			let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
+			this.setState({ stakingBalance: stakingBalance.toString()})
+		} else {
+			window.alert('Error! DecntralBank contract not deployed - no detected network!')	
+		}
+		this.setState({ loading: false })
 	}
 
 	constructor(props) {
@@ -52,8 +74,8 @@ class App extends Component {
 			tether: {},
 			rwd: {},
 			decentralBank: {},
-			tetherBalance: '0',
-			rwdBalance: '0',
+			tetherBalance: 'zero',
+			rwdBalance: 'zero',
 			stakingBalance: '0',
 			loading: true
 		}
@@ -66,6 +88,7 @@ class App extends Component {
 			<Navbar account={this.state.account}/>
 			<div className="text-center" >
 				<h1>
+					{console.log(this.state.loading)}
 				</h1>
 			</div>
 			<div>
