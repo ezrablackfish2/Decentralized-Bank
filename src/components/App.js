71677d3d07cf93e7b1c773e2e9141e64c8736c9d
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-03-14 09:57:45 +0300

    finally i will do it myself

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
 import './App.css';
 import Navbar from './Navbar';
+import Web3 from 'web3'
+import Tether from  '../truffle_abis/Tether.json';
 
 
 class App extends Component {
 
+
+	async UNSAFE_componentWillMount() {
+		await this.loadWeb3()
+		await this.loadBlockchainData()
+	}
+
+	async loadWeb3() {
+		if (window.ethereum) {
+			window.web3 = new Web3(window.ethereum)
+			await window.ethereum.enable()
+		} else if (window.web3) {
+			window.web3 = new Web3(window.web3.currentProvider)
+		}
+		  else {
+			  window.alert('No etheruem browser detected! check out MetaMask')
+			
+		}
+	}
+
+	async loadBlockchainData() {
+		const web3 = window.web3
+		const account = await web3.eth.getAccounts()
+		this.setState({ account: account[1] })
+		const networkId = await web3.eth.net.getId()
+
+		
+		const tetherData = Tether.networks[networkId]
+		if (tetherData) {
+			const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
+			this.setState({ tether })
+			let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
+			this.setState({tetherBalance: tetherBalance.toString()})
+			console.log({balance: tetherBalance});
+		} else {
+			window.alert('Error! Tether contract not deployed - no detected network!')	
+		}
+	}
+
 	constructor(props) {
 		super(props)
 		this.state = {
-			account: '0x0'
+			account: '0x0',
+			tether: {},
+			rwd: {},
+			decentralBank: {},
+			tetherBalance: '0',
+			rwdBalance: '0',
+			stakingBalance: '0',
+			loading: true
 		}
 	}
 	render() {
@@ -18,8 +65,8 @@ class App extends Component {
 				 }}>
 			<Navbar account={this.state.account}/>
 			<div className="text-center" >
-				<h>
-				</h>
+				<h1>
+				</h1>
 			</div>
 			<div>
 				hello
